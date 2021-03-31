0174fc05291abc45bd30e6c0ce6caea4e40b76d2
Adrian Kolano <kolanoadrian@hotmail.com>
2021-03-31 16:26:33 +0200

    add coments

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 {
+    // min 16:32 c.d
+    // przywitanie
     const SayHello = () => {
         console.log("Welcome in To to list")
     }
+    // załadowanie strony mogło by być przywitaniem
     window.addEventListener("load", function () {
         console.log("The page has loaded")
     });
+    // lista z objektami 
     const ListWithObjects = [
         {
             volume: "wykonać listę zadań samodzielnie",
@@ -15,20 +19,30 @@
             done: true,
         },
     ];
-
+// renderowanie 
     const render = () => {
         let htmlString = ""
         for (ListWithObject of ListWithObjects) {
             htmlString += `
             <li ${ListWithObject.done ? "style= \"text-decoration: line-through\"" : ""}>
-
+            <button class="js-remove">usuń</button>
             ${ListWithObject.volume}
 
             </li>
             `;
         };
+        //  iterowanie forEach i usuwanie -- splice zadń
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        const removeButtons = document.querySelectorAll(".js-remove");
+        console.log(removeButtons);
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                ListWithObjects.splice(index, 1);
+                render();
+            });
+        });
     };
+    // tu łapiemy input
     const onFormSubmit = (event) => {
         event.preventDefault();
         const newTaskVolume = document.querySelector(".js-newTask").value.trim();
@@ -38,6 +52,7 @@
         }
         addNewTask(newTaskVolume);
     }
+    // pusch dodawanie do listy
      const addNewTask = (newTaskVolume) => {
         ListWithObjects.push({
             volume: newTaskVolume,
@@ -48,6 +63,7 @@
     const init = () => {
         SayHello();
         render();
+        // tu wczeńniej była finkcja onFormSubmit po submit
         const form = document.querySelector(".js-form");
         form.addEventListener("submit", onFormSubmit );
 
