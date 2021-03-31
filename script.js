c1aca2acecb06411221b811b05597dc69918c76e
Adrian Kolano <kolanoadrian@hotmail.com>
2021-03-31 16:08:33 +0200

    adding new tasks and showing if a task is done

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
     const SayHello = () => {
         console.log("Welcome in To to list")
     }
-
+    window.addEventListener("load", function () {
+        console.log("The page has loaded")
+    });
     const ListWithObjects = [
         {
             volume: "wykonać listę zadań samodzielnie",
@@ -18,20 +20,36 @@
         let htmlString = ""
         for (ListWithObject of ListWithObjects) {
             htmlString += `
-            <li ${ListWithObject.done ? "style= \"text-decoration: line-through\"" :""}>
+            <li ${ListWithObject.done ? "style= \"text-decoration: line-through\"" : ""}>
 
             ${ListWithObject.volume}
-            
+
             </li>
             `;
         };
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskVolume = document.querySelector(".js-newTask").value.trim();
+        console.log(newTaskVolume);
+        if ( newTaskVolume === "") {
+            return;
+        }
+        addNewTask(newTaskVolume);
+    }
+     const addNewTask = (newTaskVolume) => {
+        ListWithObjects.push({
+            volume: newTaskVolume,
+        })
+        render();
+     }
 
     const init = () => {
         SayHello();
         render();
-
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit );
 
     }
     init();
